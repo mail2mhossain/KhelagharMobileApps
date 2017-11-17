c476dd0fc18975dd003f5f2bc2e5b45c92e4086b
Smarthome iot <smarthomeiot@outlook.com>
2017-11-17 19:06:01 +0600

    navigation issue has been fixed

--- KhelagharMobileApps/KhelagharMobileApps/KhelagharMobileApps/ViewModels/MainPageViewModel.cs
+++ KhelagharMobileApps/KhelagharMobileApps/KhelagharMobileApps/ViewModels/MainPageViewModel.cs
@@ -86,7 +86,7 @@ namespace KhelagharMobileApps.ViewModels
           {
             NavigationParameters param = new NavigationParameters();
             param.Add("show", selected.Item);
-            await _navigationService.NavigateAsync("/MasterPage/NavigationPage/DetailPage", param);
+            await _navigationService.NavigateAsync("MasterPage/DetailPage", param);
           });
         }
         return _goToDetailPage;
